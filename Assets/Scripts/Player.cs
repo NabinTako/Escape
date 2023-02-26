8e34df84d3a326453d143e95618b477613974bd7
NabinTako <nabintako999@gmail.com>
2023-02-26 16:39:55 +0545

    Combact system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,38 +6,108 @@ public class Player : MonoBehaviour
 {
     float horizontalInput, VerticalInput;
     public float xspeed, yspeed;
-    Vector3 motion = Vector3.zero;
+    Vector3 motionX = Vector3.zero;
+    Vector3 motionY = Vector3.zero;
+    bool canMoveX = true;
+    bool canMoveY = true;
+
+    //references
+    Animator anim;
     private void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
     void Update()
     {
         Movement();
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Attack");
+        }
     }
-   private void Movement()
+    private void Movement()
     {
         //Takes the inputs from the user
         horizontalInput = Input.GetAxisRaw("Horizontal");
         VerticalInput = Input.GetAxisRaw("Vertical");
 
-        
-        motion = new Vector3(horizontalInput * xspeed, VerticalInput * yspeed, 0);
-        
+
+        motionX = new Vector3(horizontalInput * xspeed,0, 0);
+        motionY = new Vector3(0, VerticalInput * yspeed, 0);
+
         // Turn player left or right.
-        if(horizontalInput < 0)
+       bool[] result = CheckCollision();
+        // To move the player to the desired position
+        if (result[0])
         {
+            transform.Translate(motionX * Time.deltaTime);
+        }
+        if (result[1])
+        {
+            transform.Translate(motionY * Time.deltaTime);
+        }
+
+    }
+
+    // Checking if hte player can move or not on a specified axis.
+    private bool[] CheckCollision()
+    {
+        RaycastHit2D hitx, hity;
+
+        //Checking for collision on X axis.
+        if (horizontalInput < 0)
+        {
+            hitx = Physics2D.Raycast(transform.position, Vector3.left, 0.09f);
+            //Debug.DrawRay(transform.position, Vector3.left * 0.09f, Color.green,0.2f);
             transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (horizontalInput > 0)
+        {
+
+            hitx = Physics2D.Raycast(transform.position, Vector3.right, 0.09f);
+            //Debug.DrawRay(transform.position, Vector3.right * 0.09f, Color.green,0.2f);
+            transform.localScale = new Vector3(1, 1, 1);
         }
         else
         {
-
-            transform.localScale = new Vector3(1, 1, 1);
+            hitx = Physics2D.Raycast(transform.position, Vector3.zero);
         }
 
-        RaycastHit2D hit;
-        // To move the player to the desired position
-        transform.Translate(motion * Time.deltaTime);
+        //Checking for collision on Y axis.
+        if (VerticalInput < 0)
+        {
+            hity = Physics2D.Raycast(transform.position, Vector3.down, 0.12f);
+            //Debug.DrawRay(transform.position, Vector3.down * 0.12f, Color.green, 0.2f);
+        }
+        else if (VerticalInput > 0)
+        {
+
+            hity = Physics2D.Raycast(transform.position, Vector3.up, 0.12f);
+            //Debug.DrawRay(transform.position, Vector3.up * 0.12f, Color.green, 0.2f);
+        }
+        else
+        {
+            hity = Physics2D.Raycast(transform.position, Vector3.zero); 
+        }
+
+        if (hitx.collider != null && hitx.collider.tag == "Blocking")
+        {
+            canMoveX = false;
+        }
+        else
+        {
+            canMoveX = true;
+        }
+        if (hity.collider != null && hity.collider.tag == "Blocking")
+        {
+            canMoveY = false;
+        }
+        else
+        {
+            canMoveY = true;
+        }
+        bool[] result = {canMoveX, canMoveY};
+        return result;
     }
 
 }
